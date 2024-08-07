46fda285f5ed60f4dd634c442e648ed9d097bf19
Muhammad Hammad <itshammadofficial@gmail.com>
2024-08-08 00:09:48 +0500

    add score to track number of correct questions

--- 01_basics/quiz.py
+++ 01_basics/quiz.py
@@ -6,23 +6,30 @@ if playing.lower() == "yes":
 
     print("Okay! Let's play")
     
+    score = 0
+
     answer = input("What does CPU stand for? ")
     if answer == "central processing unit":
         print("Correct!")
+        score += 1
     else:
         print("Incorrect! The correct answer is Central Processing Unit.")
     
     answer = input("What does GPU stand for? ")
     if answer == "graphics processing unit":
         print("Correct!")
+        score += 1
     else:
         print("Incorrect! The correct answer is Graphics Processing Unit.")
     
     answer = input("What does RAM stand for? ")
     if answer == "random access memory":
         print("Correct!")
+        score += 1
     else:
         print("Incorrect! The correct answer is Random Access Memory.")
 
+    print("You got " + str(score) + " questions correct.")
 else:
+    print("Goodbye!")
     quit()
