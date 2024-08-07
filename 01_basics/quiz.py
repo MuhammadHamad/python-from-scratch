9166af692981430c10ae84e2becd0b9942b0541e
Muhammad Hammad <itshammadofficial@gmail.com>
2024-08-07 20:57:43 +0500

    add 1 more question

--- 01_basics/quiz.py
+++ 01_basics/quiz.py
@@ -2,19 +2,27 @@ print("Welcome to the console QUIZ...")
 
 playing = input("Do you want to play? ")
 
-if playing.lower() != "yes":
+if playing.lower() == "yes":
+
+    print("Okay! Let's play")
+    
+    answer = input("What does CPU stand for? ")
+    if answer == "central processing unit":
+        print("Correct!")
+    else:
+        print("Incorrect! The correct answer is Central Processing Unit.")
+    
+    answer = input("What does GPU stand for? ")
+    if answer == "graphics processing unit":
+        print("Correct!")
+    else:
+        print("Incorrect! The correct answer is Graphics Processing Unit.")
+    
+    answer = input("What does RAM stand for? ")
+    if answer == "random access memory":
+        print("Correct!")
+    else:
+        print("Incorrect! The correct answer is Random Access Memory.")
+
+else:
     quit()
-
-print("Okay! Let's play")
-
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
-    print("Correct!")
-else:
-    print("Incorrect! The correct answer is Central Processing Unit.")
-
-answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
-    print("Correct!")
-else:
-    print("Incorrect! The correct answer is Graphics Processing Unit.")
